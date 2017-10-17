0df35897ebb5ea6623cb4d5bf45991a3b2c40170
jsalamat <salamat_jon@yahoo.com>
2017-10-17 13:53:08 -0400

    animation not syncing properly with threemin and aquarellepass

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,2 +1,10 @@
-alert("Javascript and jQuery works!");
-console.log('you can erase this if alert works');
+var fade = document.querySelector('.fade');
+var topNav = document.querySelector('.top');
+var title = document.querySelector('.title');
+var title = document.querySelector('.icon');
+var image = document.getElementById('watercolor');
+
+var aquarelle = new Aquarelle(image, 'assets/images/mask.png', {
+	autoplay: true,
+	loop: false
+});
