e4d957d54f15571a9b4b15f396ca92d15e0057b8
jsalamat <salamat_jon@yahoo.com>
2017-10-18 23:49:46 -0400

    create a fade content by swapping to remove method

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,11 @@ var aquarelle = new Aquarelle(image, 'assets/images/mask.png', {
 	autoplay: true,
 	loop: false
 });
+
+aquarelle.addEventListener('created', function() {
+	fade.style.opacity = '1';
+	var canvas = this.getCanvas();
+	canvas.removeAttribute('style');
+	image.parentNode.insertBefore(canvas, image.nextSibling);
+	image.parentNode.removeChild(image);
+});
