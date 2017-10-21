aeffbbc19aea1dd8a415fe4502ff0a7e15888cf1
jsalamat <salamat_jon@yahoo.com>
2017-10-21 00:21:48 -0400

    title to fade into animation with icon faded and blur

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 var fade = document.querySelector('.fade');
 var topNav = document.querySelector('.top');
 var title = document.querySelector('.title');
-var title = document.querySelector('.icon');
+var icon = document.querySelector('.icon');
 var image = document.getElementById('watercolor');
 
 var aquarelle = new Aquarelle(image, 'assets/images/mask.png', {
@@ -20,20 +20,11 @@ aquarelle.addEventListener('created', function() {
 aquarelle.addEventListener('changed', function(event) {
 	topNav.style.opacity = this.transitionInRange(0, 1, 5000, 8000);
 	var canvas = this.getCanvas();
-	canvas.style.webkitTransform = canvas.style;
-	transform = 'translate(-50%, -50%) scale('+ this.transitionInRange(.75, 1) + ')'
-});
+	canvas.style.webkitTransform = canvas.style.transform = 'translate(-50%, -50%) scale('+ this.transitionInRange(.75, 1) + ')';
 
-title.style.opacity = this.
-transitionInRange(0, 1, 0, 2500);
-title.style.webkitTransform = title.style.
-transform = 'scale(' + this.
-transitionInRange(.8, 1, 0, 4500) + ')';
-icon.style.webkitFilter = 'blur(' + event.
-target.transitionInRange(4, 0, 3500, 4500) + 'px)';
-icon.style.opacity = event.target.
-transitionInRange(0, 1, 3500, 4500);
-icon.style.webkitTransform = icon.style.
-transform = 'scale(' + event.target.
-transitionInRange(1.5, 1, 3500, 5000) + ')';
+	title.style.opacity = this.transitionInRange(0, 1, 0, 2500);
+	title.style.webkitTransform = title.style.transform = 'scale(' + this.transitionInRange(.8, 1, 0, 4500) + ')';
+	icon.style.webkitFilter = 'blur(' + event.target.transitionInRange(4, 0, 3500, 4500) + 'px)';
+	icon.style.opacity = event.target.transitionInRange(0, 1, 3500, 4500);
+	icon.style.webkitTransform = icon.style.transform = 'scale(' + event.target.transitionInRange(1.5, 1, 3500, 5000) + ')';
 });
