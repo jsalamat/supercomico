2f7ed7782476219bedb3b9b6f12ff7e232d52f32
jsalamat <salamat_jon@yahoo.com>
2017-10-20 23:59:19 -0400

    animation adjustments need fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,9 +18,22 @@ aquarelle.addEventListener('created', function() {
 });
 
 aquarelle.addEventListener('changed', function(event) {
-	topNav.style.opacity = this.
-	transitionInRange(0, 1, 5000, 8000);
+	topNav.style.opacity = this.transitionInRange(0, 1, 5000, 8000);
 	var canvas = this.getCanvas();
 	canvas.style.webkitTransform = canvas.style;
 	transform = 'translate(-50%, -50%) scale('+ this.transitionInRange(.75, 1) + ')'
-})
+});
+
+title.style.opacity = this.
+transitionInRange(0, 1, 0, 2500);
+title.style.webkitTransform = title.style.
+transform = 'scale(' + this.
+transitionInRange(.8, 1, 0, 4500) + ')';
+icon.style.webkitFilter = 'blur(' + event.
+target.transitionInRange(4, 0, 3500, 4500) + 'px)';
+icon.style.opacity = event.target.
+transitionInRange(0, 1, 3500, 4500);
+icon.style.webkitTransform = icon.style.
+transform = 'scale(' + event.target.
+transitionInRange(1.5, 1, 3500, 5000) + ')';
+});
